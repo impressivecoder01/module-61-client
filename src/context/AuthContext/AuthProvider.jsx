3215cfa670a4ca8ad6f9de0e6b7536a4220ffcea
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-30 01:13:37 +0600

    logut remove token failed

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -35,11 +35,22 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('state captured', currentUser)
-            axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
-            .then(res => {
-                console.log(res.data)
-            })
-            setLoading(false);
+            if(currentUser?.email){
+                const user = {email: currentUser.email};
+                axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
+                .then(res => {
+                    console.log(res.data)
+                    setLoading(false);
+                })
+            }
+            else{
+                axios.post('http://localhost:5000/logout', {withCredentials: true})
+                .then(res => {
+                    console.log('logout', res.data)
+                    setLoading(false)
+                })
+            }
+            
         })
 
         return () => {
